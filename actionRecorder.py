2e59a900c2868a2fd049b82ad9cda95c217a9106
Taras <mandryk.taras@gmail.com>
2017-12-04 14:57:40 -0500

    Update actionRecorder.py

--- actionRecorder.py
+++ actionRecorder.py
@@ -22,23 +22,8 @@ while True:
 
     if a != state_left:
         state_left = a
-        #print(a)
         if a < 0:
-            #print("left button clicked")
             mouseClickLocations.append(pyautogui.position())
-            #print(mouseClickLocations)
-        #else:
-        #    print("left button released")
-
-    #if keyboard.is_pressed('a') and mouseClickLocations != []:
-    #    for locations in mouseClickLocations:
-            
-            ###move mouse to location and open file
-            #win32api.SetCursorPos((locations[0], locations[1]))
-
-            ###open the file
-            #pyautogui.click(clicks=2)
-     #       print(locations)
 
     
     if keyboard.is_pressed('a'):    ###try to get an actual key binding
@@ -53,8 +38,7 @@ while True:
 
             ###get window title (ie. file name)
             windowTitle = win32gui.GetWindowText(win32gui.GetForegroundWindow())
-            print(windowTitle)
-
+            
             filename = windowTitle.split(".pdf")[0] + ".pdf"
             path = "C:/Users/Taras/Desktop/PDFs/"
             fp = open(path + filename, 'rb')
@@ -63,7 +47,6 @@ while True:
 
             ###PDF title
             pdfTitle = (doc.info[0]['Title']).decode("utf-8")
-            print(pdfTitle)  # The "Info" metadata
 
             ###close the PDF window
             subprocess.call(["taskkill", "/f", "/im", "ACROBAT.EXE"])
@@ -75,7 +58,6 @@ while True:
             app_dialog.Minimize()
             app_dialog.Restore()
 
-            #app.Notepad.Edit.set_edit_text(u"I am typing s\xe4me text to Notepad\r\n\r\n")
             app.Notepad.Edit.type_keys(u"{END}{ENTER}" + pdfTitle, with_spaces = True)
 
             app_dialog.Minimize()
@@ -90,7 +72,6 @@ while True:
 
                 ###get window title (ie. file name)
                 windowTitle = win32gui.GetWindowText(win32gui.GetForegroundWindow())
-                print(windowTitle)
 
                 filename = windowTitle.split(".pdf")[0] + ".pdf"
                 path = "C:/Users/Taras/Desktop/PDFs/"
@@ -100,7 +81,6 @@ while True:
 
                 ###PDF title
                 pdfTitle = (doc.info[0]['Title']).decode("utf-8")
-                print(pdfTitle)  # The "Info" metadata
 
                 ###close the PDF window
                 subprocess.call(["taskkill", "/f", "/im", "ACROBAT.EXE"])
